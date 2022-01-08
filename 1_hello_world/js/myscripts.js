6627a164eef14d80f523059d299853dbe7f83902
world324digital <70345255+world324digital@users.noreply.github.com>
2022-01-08 18:15:19 +0300

    template and hello world

---
+++ 1_hello_world/js/myscripts.js
@@ -0,0 +1,13 @@
+try {
+	function helloWorld(){
+  	document.getElementById("go").innerHTML = "Hello World!";
+
+	helloWorld();
+}
+ 	// statements
+ } catch(e) {
+ 	// statements
+   console.log(e);
+ } 
+
+ 
